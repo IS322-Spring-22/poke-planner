221f137c5067812b2657c4e3a776a2dd52b3a74e
Dale <djs93@njit.edu>
2022-05-11 02:52:58 -0400

    Add capitalization to pokemon name in team

--- src/components/TeamMemberList.js
+++ src/components/TeamMemberList.js
@@ -1,12 +1,13 @@
 import React from "react";
 import TeamMember from "./TeamMember";
+import {capitalize} from "../services/api";
 
 // change member.moves to member.moveList once api connected
 
 const TeamMemberList = ({ team, removePokemon, editPokemon }) => {
     const teamList = team.map((member,i) => (
         <TeamMember
-            name={member.name}
+            name={capitalize(member.name)}
             sprite={member.sprites.front_default}
             types={member.types}
             moves={member.moveList}
