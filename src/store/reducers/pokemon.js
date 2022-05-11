230e5e2352ba1777b3e8af8dd36939bdb9229bf1
Dale <djs93@njit.edu>
2022-05-10 23:03:23 -0400

    Create pokemon reducer file with default state

---
+++ src/store/reducers/pokemon.js
@@ -0,0 +1,8 @@
+import { ADD_POKEMON, REMOVE_POKEMON, EDIT_POKEMON, ADD_MOVE, REMOVE_MOVE } from "../actionTypes";
+
+const DEFAULT_STATE = {
+    team: [],
+    numPokemon: 0,
+    lastPokemon: {},
+    teamTypes: []
+}
