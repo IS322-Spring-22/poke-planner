4b30c456a39085b5c8c6f0ba383710ce41f6c7d4
Dale <djs93@njit.edu>
2022-05-11 04:29:05 -0400

    Put setCurrentPage update in useEffect

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import TeamMemberList from "../components/TeamMemberList";
 import SelectPokemon from "../components/SelectPokemon";
 import {editPokemonInTeam, removePokemonFromTeam} from "../store/actions/pokemon";
@@ -6,7 +6,9 @@ import {withRouter} from "react-router-dom";
 import {connect, useSelector} from "react-redux";
 
 function Team({ setCurrentPage}) {
-  setCurrentPage("Teams");
+    useEffect(() => { setCurrentPage("Teams");}, []);
+
+
     const team = useSelector(state => state.teams.team);
     return (
         <div>
