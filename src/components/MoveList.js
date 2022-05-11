bc895d5a1d4b9e9c1826cbcc81536c1120315f86
Dale <djs93@njit.edu>
2022-05-11 02:42:44 -0400

    Create MoveList component

---
+++ src/components/MoveList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { capitalize } from "../services/api";
+
+const Move = ({moveName}) => (
+    <div className="move">{capitalize(moveName)}</div>
+);
+
+const MoveList = props => {
+    const {moves} = props;
+    const moveList = moves.map( (move,i) => (
+        <Move
+            moveName={move}
+            key={i}
+        />
+    ));
+    return (
+        <div>
+            {moveList}
+        </div>
+    )
+}
+
+export default MoveList;
