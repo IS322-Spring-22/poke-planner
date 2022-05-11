03ab2f517a6d86b148d63cd53877a2915471c2bf
Dale <djs93@njit.edu>
2022-05-11 02:43:47 -0400

    Create Team Member List component

---
+++ src/components/TeamMemberList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import TeamMember from "./TeamMember";
+
+// change member.moves to member.moveList once api connected
+
+const TeamMemberList = ({ team, removePokemon, editPokemon }) => {
+    const teamList = team.map((member,i) => (
+        <TeamMember
+            name={member.name}
+            sprite={member.sprites.front_default}
+            types={member.types}
+            moves={member.moveList}
+            removePokemon={removePokemon.bind(this, member)}
+            editPokemon={editPokemon.bind(this, member)}
+            key={i}
+        />
+    ));
+    return (
+        <div className="row">
+            {teamList}
+        </div>
+    );
+}
+
+export default TeamMemberList;
