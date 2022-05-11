e5a97a95c37330837e4c965ec582123575372470
Dale <djs93@njit.edu>
2022-05-10 23:32:53 -0400

    Create team and move actions

---
+++ src/store/actions/pokemon.js
@@ -0,0 +1,65 @@
+import {ADD_MOVE, ADD_POKEMON, EDIT_POKEMON, REMOVE_MOVE, REMOVE_POKEMON} from "../actionTypes";
+import {getMoves, getPokemon} from "../../services/api";
+import {addError} from "./errors";
+
+export const addPokemon = pokemon => ({
+    type: ADD_POKEMON,
+    pokemon
+});
+
+export const removePokemon = pokemon => ({
+    type: REMOVE_POKEMON,
+    pokemon
+});
+
+export const editPokemon = pokemon => ({
+    type: EDIT_POKEMON,
+    pokemon
+});
+
+export const addMove = (pokemon, move) => ({
+    type: ADD_MOVE,
+    pokemon,
+    move
+});
+
+export const removeMove = (pokemon, move) => ({
+    type: REMOVE_MOVE,
+    pokemon,
+    move
+});
+
+export const addPokemonToTeam = id => {
+    return dispatch => {
+        return getPokemon(id)
+            .then( async function(pokemon) {
+                pokemon["moveList"] = [];
+                pokemon.moves = await getMoves(pokemon.moves);
+                dispatch( addPokemon(pokemon) );
+            }).catch( err => dispatch( addError(err) ));
+    }
+}
+
+export const removePokemonFromTeam = pokemon => {
+    return async function(dispatch) {
+        await dispatch(removePokemon(pokemon));
+    }
+}
+
+export const editPokemonInTeam = pokemon => {
+    return dispatch => {
+        dispatch(editPokemon(pokemon));
+    }
+}
+
+export const addMoveToList = (pokemon, move) => {
+    return dispatch => {
+        dispatch(addMove(pokemon, move));
+    }
+}
+
+export const removeMoveFromList = (pokemon, move) => {
+    return dispatch => {
+        dispatch(removeMove(pokemon, move));
+    }
+}
