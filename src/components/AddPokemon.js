8ce1a1a1af69296b003d98fda58d78077e77760e
Dale <djs93@njit.edu>
2022-05-11 17:12:00 -0400

    Responsive Styling for AddPokemon

--- src/components/AddPokemon.js
+++ src/components/AddPokemon.js
@@ -12,10 +12,10 @@ const AddPokemon = () => {
     });
     const moveList = useSelector((state) => state.teams.lastPokemon.moveList);
     return (
-        <div className="container mt-2">
+        <div className="addPokeContainter mt-2">
             <h1 className="d-flex justify-content-center pokeName"> Moveset: {capitalize(pokemon.name)} </h1>
             <h3 className="d-flex justify-content-center text-center"> Click on moves to select them. Click again to remove them. </h3>
-            <div className="d-md-flex justify-content-center">
+            <div className="pokeInfoDiv justify-content-center">
                 <div id="pokeInfo">
                     <img className="rounded addTeamMemberSprite" src={pokemon.sprites.front_default || DefaultSprite} alt="Pokemon Sprite" style={{
                         width: "256 px",
